f5a096cb90fdc96d888da5c3e134bbb51477ab2e
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-11-14 19:23:08 +0530

    Added:Ability to find cheapest hotel for the given range

--- src/main/java/com/bridzelabz/HotelReservationSystem.java
+++ src/main/java/com/bridzelabz/HotelReservationSystem.java
@@ -2,24 +2,35 @@ package com.bridzelabz;
 import java.util.ArrayList;
 import java.util.List;
 /**
- * Purpose  - To build a Hotel Reservation System
+ * Purpose  - Ability to find cheapest hotel for a given data range
  * 
  */
 
 public class HotelReservationSystem {
-	List<Hotel> hotels = new ArrayList<>();
-	/**
-	 * This method is used to add hotel to the Hotel List
-	 */
-	public boolean addHotel(Hotel hotel) {
-		return hotels.add(hotel);
-	}
+	private List<Hotel> hotels;
 
+    public HotelReservationSystem(){
+        this.hotels = new ArrayList<Hotel>();
+    }
 
-	public static void main(String[] args){
+    /**
+     * This method is used to add hotel to the Hotel List
+     */
+    public boolean addHotel(Hotel hotel) {
+        return this.hotels.add(hotel);
+    }
 
-		HotelReservationSystem hotel = new HotelReservationSystem();
-		hotel.addHotel(null);
-	}
+    /**
+     * This method is used to find the cheapest hotel in the HotelList
+     * @return - cheapestHotel - The cheapest hotel in the list
+     */
+    public int findCheapestHotel(){
+        int LakeWoodRate = 110; int BridgeWoodRate = 160; int Ridgewood = 220;
+        int cheapestHotel = (LakeWoodRate>BridgeWoodRate) ?
+                (LakeWoodRate>Ridgewood ? LakeWoodRate : Ridgewood) :
+                (BridgeWoodRate>Ridgewood ? BridgeWoodRate : Ridgewood) ;
+        System.out.println("The cheapest Hotel is " +cheapestHotel);
+        return cheapestHotel;
+    }
 }
 
--- src/test/java/com/bridzelabz/HotelReservationTest.java
+++ src/test/java/com/bridzelabz/HotelReservationTest.java
@@ -4,14 +4,23 @@ import org.junit.Test;
 
 public class HotelReservationTest {
 	HotelReservationSystem hotelReservation = new HotelReservationSystem();
-
 	@Test
-	public void whenAddedHotel_ShouldReturn_AddedHotel() {
-		boolean isValid1 = hotelReservation.addHotel( new Hotel("LakeWood",110));
-		Assert.assertTrue(isValid1);
-		boolean isValid2 = hotelReservation.addHotel( new Hotel("Bridgewood",160));
-		Assert.assertTrue(isValid2);
-		boolean isValid3 = hotelReservation.addHotel( new Hotel("Ridgewood",220));
-		Assert.assertTrue(isValid3);
-	}
+    public void whenAddedHotel_ShouldReturn_AddedHotel() {
+        try{
+            boolean isValid1 = hotelReservation.addHotel( new Hotel("LakeWood",110));
+            Assert.assertTrue(isValid1);
+            boolean isValid2 = hotelReservation.addHotel( new Hotel("Bridgewood",160));
+            Assert.assertTrue(isValid2);
+            boolean isValid3 = hotelReservation.addHotel( new Hotel("Ridgewood",220));
+            Assert.assertTrue(isValid3);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void toFindTheChepeastHotel() {
+        HotelReservationSystem cheapHotel = new HotelReservationSystem();
+        cheapHotel.findCheapestHotel();
+    }
 }
